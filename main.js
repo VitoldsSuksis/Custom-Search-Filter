761b77353e23ab5c054009250c4f80474fe9b63f
Vitolds <vsuksis@gmail.com>
2020-01-03 15:13:26 +0200

    Custom Search Filter

---
+++ main.js
@@ -0,0 +1,17 @@
+function filter() {
+
+    var value, name, profile, i;
+
+    value = document.getElementById("value").value.toUpperCase();
+    profile = document.getElementsByClassName("profile");
+
+    for (i = 0; i < profile.length; i++) {
+        name = profile[i].getElementsByClassName("name");
+
+        if (name[0].innerHTML.toUpperCase().indexOf(value) > -1) {
+            profile[i].style.dispaly = "flex";
+        } else {
+            profile[i].style.dispaly = "none";
+        }
+    }
+}
